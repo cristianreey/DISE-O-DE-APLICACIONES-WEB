d35b3424d85ca5ddbad70d4b43cb8d537a832dc1
cristianreey <cris11salle@gmail.com>
2023-12-08 15:58:36 +0100

    ññ

--- SALUD AL INSTANTE/script.js
+++ SALUD AL INSTANTE/script.js
@@ -44,3 +44,66 @@ document.addEventListener("DOMContentLoaded", function () {
     alert("Cita confirmada. ¡Gracias!");
   });
 });
+
+//Creamos el evento para que se habran las observaciones cuando hagamos clic
+document.addEventListener("DOMContentLoaded", function () {
+  const observacionIcono = document.querySelector(".observacion");
+  const observacionesContainer = document.getElementById("observaciones-container");
+
+  observacionIcono.addEventListener("click", function () {
+    observacionesContainer.classList.toggle("mostrar-observaciones");
+  });
+});
+function toggleObservaciones() {
+  var observacionesContainer = document.getElementById("observaciones-container");
+  observacionesContainer.classList.toggle("mostrar");
+}
+
+function mostrarFormulario() {
+  var formularioObservacion = document.getElementById("formularioObservacion");
+  formularioObservacion.style.display = "block";
+}
+
+function enviarObservacion() {
+  // Obtén el valor de la valoración seleccionada
+  var valoracion = obtenerValoracion();
+
+  // Aquí puedes obtener los valores del formulario y hacer lo que necesites con ellos
+  var nombreUsuario = document.getElementById("nombreUsuario").value;
+  var textoObservacion = document.getElementById("textoObservacion").value;
+
+  // Cambia el color de las estrellas seleccionadas
+  var estrellasSeleccionadas = document.querySelectorAll('input[name="valoracion"]:checked + label');
+  estrellasSeleccionadas.forEach(function (label) {
+    label.style.color = "gold";
+  });
+
+  // Añade lógica para mostrar la observación en el lugar adecuado
+  var observacionesContainer = document.getElementById("observaciones-container");
+  var nuevaObservacion = document.createElement("div");
+  nuevaObservacion.classList.add("div-observaciones");
+  nuevaObservacion.innerHTML = `
+    <p>${nombreUsuario} <span style="color: gold;">${"\u2605".repeat(parseInt(valoracion))}</span>${"\u2606".repeat(
+    5 - parseInt(valoracion)
+  )}</p>
+    <div style="border: 1px solid #ccc; padding: 10px; border-radius: 20px; margin-bottom: 10px;"> ${textoObservacion}</div>`;
+  observacionesContainer.appendChild(nuevaObservacion);
+
+  // Oculta el formulario después de enviar la observación
+  var formularioObservacion = document.getElementById("formularioObservacion");
+  formularioObservacion.style.display = "none";
+}
+
+function obtenerValoracion() {
+  var valoracionRadios = document.querySelectorAll('input[name="valoracion"]');
+  var valoracionSeleccionada;
+
+  for (var i = 0; i < valoracionRadios.length; i++) {
+    if (valoracionRadios[i].checked) {
+      valoracionSeleccionada = valoracionRadios[i].value;
+      break;
+    }
+  }
+
+  return valoracionSeleccionada;
+}
